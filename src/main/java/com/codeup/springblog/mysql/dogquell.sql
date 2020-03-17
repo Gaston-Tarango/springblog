79a20fc1e38524eacbc06c970ae984b2007b191e
Gaston Tarango <gaston.tarango@gmail.com>
2020-03-17 16:08:48 -0500

    .

---
+++ src/main/java/com/codeup/springblog/mysql/dogquell.sql
@@ -0,0 +1,17 @@
+drop database doglister_db;
+
+USE doglister_db;
+
+        INSERT INTO dogs (age, name, reside_state)
+        VALUES
+        (2, 'Chuck', 'TX'),
+        (5, 'Fred', 'OH'),
+        (3, 'Bud', 'TN'),
+        (10, 'Bailey', 'AL'),
+        (11, 'Lexie', 'TX'),
+        (1, 'Snickers', 'TX'),
+        (6, 'Red', 'FL'),
+        (8, 'Barney', 'CA'),
+        (12, 'Bowser', 'TX');
+
+        select * from dogs;
